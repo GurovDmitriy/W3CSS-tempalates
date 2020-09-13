de78158816ef357a80109c5d04c329232925b54c
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-09-14 02:15:46 +0300

    End w3css catering page

--- source/js/default.js
+++ source/js/default.js
@@ -1,1 +1,14 @@
-'use strict';
+'use strict'
+
+const burger = document.querySelector('.usr-burger');
+const menu = document.querySelector('.usr-menu');
+
+burger.onclick = function () {
+  if (burger.classList.contains('usr-burger--open')) {
+    burger.classList.toggle('usr-burger--close');
+  } else {
+    burger.classList.add('usr-burger--open');
+  }
+
+  menu.classList.toggle('w3-hide-small');
+}
