e012b7ef5b7bc4a6123b373f4e03cb4b55ad084f
GurovDmitriy <gurovdmitriy1991@gmail.com>
2020-09-17 23:36:09 +0300

    Fix scroll spy for all w3css pages

---
+++ source/js/catering.js
@@ -0,0 +1,29 @@
+'use strict'
+// set the observer options
+let options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+}
+
+// create observer
+const observer = new IntersectionObserver( entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      // add an observed class to the section
+      entry.target.classList.add('observed');
+      // check the section's id
+      document.querySelectorAll('nav a').forEach( link => {
+        if(link.hash === `#${entry.target.id}`) {
+          link.classList.add('w3-light-grey');
+        } else {
+          link.classList.remove('w3-light-grey');
+        }
+      });
+    };
+  });
+}, options);
+// Observe all sections that have an `id` applied
+document.querySelectorAll('section[id]').forEach(section => {
+  observer.observe(section);
+});
